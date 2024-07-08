18753b85ed43795ee964578c4cded877f9031767
kviewui <kviewui@163.com>
2024-07-08 23:22:56 +0800

    feat: add v1.0.17

--- src/components/showFormDialog.js
+++ src/components/showFormDialog.js
@@ -32,6 +32,64 @@ async function checkPackenv(androidHome, javaHome) {
 	}
 }
 
+// 持久化存储配置信息的文件路径
+const moduleConfigCachePath = path.join(__dirname, '../cache/', 'module.config.json');
+const configCachePath = path.join(__dirname, '../cache/', 'config.json');
+
+async function saveCache(data, options, configuration) {
+	if (data.saveLocalConfig) {
+		const moduleConfig = {}
+		Object.keys(data).map(item => {
+			if (item.startsWith('uni-')) {
+				moduleConfig[item] = data[item]
+			}
+		})
+		localCache.set('moduleConfig', moduleConfig)
+		let cacheData = Object.assign({
+			moduleConfig: moduleConfig
+		}, {
+			saveLocalConfig: data.saveLocalConfig
+		})
+		// 判断是否按照项目独立缓存
+		if (configuration.get('kux-easy-pack-hxp.projectCacheConfig')) {
+			if (!fs.existsSync(moduleConfigCachePath)) {
+				await fs.writeFileSync(moduleConfigCachePath, JSON.stringify([], null, 2), 'utf-8')
+			}
+			let cacheDataReaded = JSON.parse(await fs.readFileSync(moduleConfigCachePath, 'utf-8'))
+			if (!cacheDataReaded) {
+				cacheDataReaded = []
+			}
+			let fsPath = localCache.get('fsPath') ?? options.uniName
+			if (cacheDataReaded.length == 0) {
+				cacheDataReaded.push({
+					fsPath: fsPath,
+					data: cacheData
+				})
+			} else {
+				if (cacheDataReaded.find(item => item.fsPath == fsPath)) {
+					cacheDataReaded.map((item, index) => {
+						if (item.fsPath === fsPath) {
+							cacheDataReaded[index] = {
+								fsPath: fsPath,
+								data: cacheData,
+							}
+						}
+					})
+				} else {
+					cacheDataReaded.push({
+						fsPath: fsPath,
+						data: cacheData
+					})
+				}
+			}
+
+			await fs.writeFileSync(moduleConfigCachePath, JSON.stringify(cacheDataReaded, null, 2), 'utf-8')
+		} else {
+			await fs.writeFileSync(configCachePath, JSON.stringify(cacheData, null, 2), 'utf-8')
+		}
+	}
+}
+
 async function showFormDialog(context) {
 
 	let webviewDialog = hx.window.createWebViewDialog({
@@ -47,7 +105,7 @@ async function showFormDialog(context) {
 	}, {
 		enableScripts: true
 	});
-	
+
 	localCache.set('webviewDialog', webviewDialog)
 
 	let webview = webviewDialog.webView;
@@ -82,7 +140,7 @@ async function showFormDialog(context) {
 			options.uniName = localCache.get('uniName');
 		}
 	}
-		
+
 	// 创建一个WebSocket服务器实例，监听在9991端口
 	const wss = new WebSocket.Server({
 		port: 9991
@@ -114,15 +172,7 @@ async function showFormDialog(context) {
 					localCache.set('storePassword', msg.data.storeForm.storePassword)
 					localCache.set('keyAlias', msg.data.storeForm.keyAlias)
 					localCache.set('keyPassword', msg.data.storeForm.keyPassword)
-					if (msg.data.saveLocalConfig) {
-						const moduleConfig = {}
-						Object.keys(msg.data).map(item => {
-							if (item.startsWith('uni-')) {
-								moduleConfig[item] = msg.data[item]
-							}
-						})
-						localCache.set('moduleConfig', moduleConfig)
-					}
+					await saveCache(msg.data, options, configuration)
 					start({
 						hx: hx,
 						uniappProjectPath: msg.data.uniName,
@@ -139,7 +189,8 @@ async function showFormDialog(context) {
 					localCache.get('webviewDialog')?.close()
 				}
 				if (msg.type === 'checkPackenv' && msg.data.localPack) {
-					const checkPackenvRes = await checkPackenv(msg.data.androidLocalSdk, msg.data.javaHome)
+					const checkPackenvRes = await checkPackenv(msg.data.androidLocalSdk, msg
+						.data.javaHome)
 					ws.send(JSON.stringify({
 						type: 'autoCheckPackenvRes',
 						data: checkPackenvRes
@@ -163,9 +214,10 @@ async function showFormDialog(context) {
 					configuration
 					.get('uts-development-android.sdkDir')
 			}))
+			const fsPath = localCache.get('fsPath') ?? options.uniName
 			ws.send(JSON.stringify({
 				type: 'uniName',
-				data: localCache.get('fsPath') ?? options.uniName
+				data: fsPath
 			}))
 			ws.send(JSON.stringify({
 				type: 'repositoryUrl',
@@ -181,33 +233,45 @@ async function showFormDialog(context) {
 				type: 'checkAgconnectServicesRes',
 				data: checkAgconnectServicesRes
 			}))
+			let configData = JSON.parse(await fs.readFileSync(configCachePath, 'utf-8'))
+			if (configuration.get('kux-easy-pack-hxp.projectCacheConfig')) {
+				configData = JSON.parse(await fs.readFileSync(moduleConfigCachePath, 'utf-8')).find(
+					item => item.fsPath = fsPath).data
+			}
+			const saveLocalConfig = configData?.saveLocalConfig ?? localCache.get('saveLocalConfig')
 			ws.send(JSON.stringify({
 				type: 'saveLocalConfig',
-				data: localCache.get('saveLocalConfig') ?? false
+				data: saveLocalConfig ?? false
 			}))
+			const moduleConfig = configData?.moduleConfig ?? localCache.get('moduleConfig')
 			ws.send(JSON.stringify({
 				type: 'moduleConfig',
-				data: localCache.get('moduleConfig') ?? {}
+				data: moduleConfig ?? {}
 			}))
 			ws.send(JSON.stringify({
 				type: 'androidPackageName',
-				data: localCache.get('androidPackageName') ?? configuration.get('kux-easy-pack-hxp.androidPackageName')
+				data: localCache.get('androidPackageName') ?? configuration.get(
+					'kux-easy-pack-hxp.androidPackageName')
 			}))
 			ws.send(JSON.stringify({
 				type: 'storePath',
-				data: localCache.get('storePath') ?? configuration.get('kux-easy-pack-hxp.storePath')
+				data: localCache.get('storePath') ?? configuration.get(
+					'kux-easy-pack-hxp.storePath')
 			}))
 			ws.send(JSON.stringify({
 				type: 'storePassword',
-				data: localCache.get('storePassword') ?? configuration.get('kux-easy-pack-hxp.storePassword')
+				data: localCache.get('storePassword') ?? configuration.get(
+					'kux-easy-pack-hxp.storePassword')
 			}))
 			ws.send(JSON.stringify({
 				type: 'keyAlias',
-				data: localCache.get('keyAlias') ?? configuration.get('kux-easy-pack-hxp.keyAlias')
+				data: localCache.get('keyAlias') ?? configuration.get(
+					'kux-easy-pack-hxp.keyAlias')
 			}))
 			ws.send(JSON.stringify({
 				type: 'keyPassword',
-				data: localCache.get('keyPassword') ?? configuration.get('kux-easy-pack-hxp.keyPassword')
+				data: localCache.get('keyPassword') ?? configuration.get(
+					'kux-easy-pack-hxp.keyPassword')
 			}))
 		} else {
 			console.log('认证失败');
@@ -232,28 +296,15 @@ async function showFormDialog(context) {
 			localCache.set('storePath', msg.data.storeForm.storePath)
 			localCache.set('keyAlias', msg.data.storeForm.keyAlias)
 			localCache.set('keyPassword', msg.data.storeForm.keyPassword)
-			if (msg.data.saveLocalConfig) {
-				const moduleConfig = {}
-				Object.keys(msg.data).map(item => {
-					if (item.startsWith('uni-')) {
-						moduleConfig[item] = msg.data[item]
-					}
-				})
-				localCache.set('moduleConfig', moduleConfig)
-			}
-			// let checkPackenvRes = null
-			// if (msg.data.autoCheckPackenv === '1') {
-			// 	checkPackenvRes = await checkPackenv(msg.data.androidLocalSdk, msg.data.javaHome)
-			// 	if (globalWs != null) {
-			// 		globalWs.send(JSON.stringify({
-			// 			type: 'autoCheckPackenvRes',
-			// 			data: checkPackenvRes
-			// 		}))
-			// 	} else {
-			// 		console.log('ws连接尚未建立');
-			// 		logger.warn('ws连接尚未建立')
-			// 	}
-			// 	return
+			await saveCache(msg.data, options, configuration)
+			// if (msg.data.saveLocalConfig) {
+			// 	const moduleConfig = {}
+			// 	Object.keys(msg.data).map(item => {
+			// 		if (item.startsWith('uni-')) {
+			// 			moduleConfig[item] = msg.data[item]
+			// 		}
+			// 	})
+			// 	localCache.set('moduleConfig', moduleConfig)
 			// }
 			wss.close(() => {
 				console.log('WebSocket 服务已停止');
